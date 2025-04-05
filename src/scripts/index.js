2bf09a7813b5d92435a1bac1a28ef0371f0bb227
Samily <samily.sss2005@gmail.com>
2025-04-05 10:16:30 -0300

    "Tarefa 4 concluída"

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+function handleModal() {
+    const buttons = document.querySelectorAll(".open--modal");
+    const modalContainer = document.querySelector("#modalController")
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            modalContainer.showModal();
+
+            closeModal();
+        });
+    });
+}
+
+function closeModal() {
+    const button = document.querySelector("#close--modal");
+    const modalContainer = document.querySelector("#modalController")
+    button.addEventListener("click", () => {
+        modalContainer.close();
+    });
+}
+handleModal();
